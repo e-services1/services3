cceeb623f0330f8b94e94acfe92dc1c767bf5c65
rbhanu1989 <rbhanu304@gmail.com>
2021-10-18 17:42:14 +0530

    login.java is add

---
+++ login.java
@@ -0,0 +1,9 @@
+public class login
+{
+        public static void main()
+        {
+                 system.out.println("login.java is printing: ");
+                 system.out.println("employee details: ");
+        }
+}
+
